b6ef29260ba53805550c254e85a013fd3e599ccd
水咲(みさき) <60120497+Misaki0331@users.noreply.github.com>
2021-12-26 19:32:49 +0900

    1.0

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SearchTextBox = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.Initwork = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.MTSearch = new System.ComponentModel.BackgroundWorker();
+            this.PasswordResultBox = new System.Windows.Forms.TextBox();
+            this.Refreshing = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SearchTextBox
+            // 
+            this.SearchTextBox.Location = new System.Drawing.Point(41, 33);
+            this.SearchTextBox.MaxLength = 14;
+            this.SearchTextBox.Name = "SearchTextBox";
+            this.SearchTextBox.Size = new System.Drawing.Size(100, 19);
+            this.SearchTextBox.TabIndex = 0;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(147, 33);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 19);
+            this.SearchButton.TabIndex = 2;
+            this.SearchButton.Text = "検索";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // Initwork
+            // 
+            this.Initwork.DoWork += new System.ComponentModel.DoWorkEventHandler(this.initwork);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Maximum = 4244113;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(800, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // MTSearch
+            // 
+            this.MTSearch.DoWork += new System.ComponentModel.DoWorkEventHandler(this.MTSearch_DoWork);
+            this.MTSearch.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.MTSearch_RunWorkerCompleted);
+            // 
+            // PasswordResultBox
+            // 
+            this.PasswordResultBox.Location = new System.Drawing.Point(41, 73);
+            this.PasswordResultBox.MaxLength = 99999999;
+            this.PasswordResultBox.Multiline = true;
+            this.PasswordResultBox.Name = "PasswordResultBox";
+            this.PasswordResultBox.ReadOnly = true;
+            this.PasswordResultBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.PasswordResultBox.Size = new System.Drawing.Size(191, 314);
+            this.PasswordResultBox.TabIndex = 4;
+            // 
+            // Refreshing
+            // 
+            this.Refreshing.Interval = 20;
+            this.Refreshing.Tick += new System.EventHandler(this.Refresh_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(247, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PasswordResultBox);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.SearchTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SearchTextBox;
+        private System.Windows.Forms.Button SearchButton;
+        private System.ComponentModel.BackgroundWorker Initwork;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker MTSearch;
+        private System.Windows.Forms.TextBox PasswordResultBox;
+        private System.Windows.Forms.Timer Refreshing;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string[] Passwords;
+        string SearchWord;
+        bool isCompleted;
+        int ProgressInt;
+        int ResultCount;
+        List<string> PasswordList= new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Loading...";
+            isCompleted = false;
+            Initwork.RunWorkerAsync();
+            while (!isCompleted)
+            {
+                Application.DoEvents();
+            }
+            Refreshing.Enabled = true;
+            this.Text = "Done";
+        }
+
+
+        private void initwork(object sender, DoWorkEventArgs e)
+        {
+            Passwords = Properties.Resources.passwords.Split('\n');
+            isCompleted = true;
+
+        }
+
+        private void MTSearch_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Searching();
+        }
+
+        void Searching()
+        {
+            PasswordList.Clear();
+            ProgressInt = 0;
+            ResultCount = 0;
+            for (int i = 0; i < Passwords.Length; i++)
+            {
+                if (Passwords[i].Contains(SearchWord))
+                {
+                    PasswordList.Add(Passwords[i]);
+                    ResultCount++;
+                }
+                ProgressInt++;
+            }
+            isCompleted = true;
+        }
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            isCompleted = false;
+            SearchButton.Enabled = false;
+            SearchWord = SearchTextBox.Text;
+            //Searching();
+            MTSearch.RunWorkerAsync();
+        }
+
+        private void MTSearch_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            SearchButton.Enabled = true;
+            isCompleted = true;
+            PasswordResultBox.Lines = PasswordList.ToArray();
+        }
+
+        private void Refresh_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+
+                progressBar1.Value = ProgressInt;
+                label1.Text = $"Found : {ResultCount}";
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
